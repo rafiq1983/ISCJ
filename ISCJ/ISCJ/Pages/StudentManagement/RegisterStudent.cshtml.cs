6764c51bbe4376a1aa5f5db68ff02d2d0cf948f8
Iftikhar Ali <Siahmed@nextgen.com>
2018-12-18 11:18:46 -0500

    Create Student Management Folder.

---
+++ ISCJ/ISCJ/Pages/StudentManagement/RegisterStudent.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ISCJ.Pages.StudentManagement
+{
+    public class RegisterStudentModel : PageModel
+    {
+    public void OnGet()
+    {
+
+    }
+
+    public void OnPost()
+    {
+      Message = "Your information has been submitted";
+    }
+
+    public string Message { get; set; }
+  
+}
+}
