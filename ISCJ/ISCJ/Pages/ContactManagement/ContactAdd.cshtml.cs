dcf4296b531c01b684e456fead941ef404093bff
Iftikhar Ali <Siahmed@nextgen.com>
2019-02-03 18:08:34 -0500

    added tab.

---
+++ ISCJ/ISCJ/Pages/ContactManagement/ContactAdd.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ISCJ.Pages.ContactManagement
+{
+    public class ContactAddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
