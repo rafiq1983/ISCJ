6ea2265d24a0a470f91855021f12477c54bcd951
Iftikhar Ali <Siahmed@nextgen.com>
2019-02-08 11:42:38 -0500

    add web api

---
+++ ISCJ/ISCJ/webapi/ContactController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLogic;
+using MA.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ISCJ.webapi
+{
+  [Route("api/[controller]")]
+  [Authorize]
+  public class ContactController : Controller
+  {
+
+    [HttpGet("Search/{prefix}")]
+    public List<Contact> PrefixSearch(string prefix)
+    {
+      ContactManager mgr = new ContactManager();
+      return mgr.GetContacts(0, 1, 100).Where(x=>(x.FirstName + " " + x.LastName).Contains(prefix)).ToList();
+    }
+    // GET: api/<controller>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+      return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<controller>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+      return "value";
+    }
+
+    // POST api/<controller>
+    [HttpPost]
+    public void Post([FromBody]string value)
+    {
+    }
+
+    // PUT api/<controller>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE api/<controller>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
+}
