dc2aec3c748bea00c38b6fddb0e205a00aed9070
Iftikhar Ali <Siahmed@nextgen.com>
2019-02-10 22:00:35 -0500

    updates.

---
+++ ISCJ/ISCJ/wwwroot/js/ContactManager.js
@@ -0,0 +1,19 @@
+﻿(function () {
+  alert('here');
+  var ContactManager = function () {
+
+    var GetContacts = function () {
+
+      return [{ "FirstName": "Mohaib" }, { "FirstName": "Wasi" }];
+
+    }
+    ContactManager.prototype.GetAllContacts = function () {
+      return GetContacts();
+    };
+
+  }
+
+
+  document.ContactManager = new ContactManager();
+
+}(document, "dev"));
