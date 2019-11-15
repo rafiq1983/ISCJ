07684f02590848eee8e16e390c3dd8665698b51e
Iftikhar Ali <i_syed2000@yahoo.com>
2019-11-15 16:41:39 -0500

    Add Invoice Types table.

---
+++ ISCJ/ISCJRegistration/dbo/Tables/InvoiceTypes.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[InvoiceTypes]
+(
+	[InvoiceTypeId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [TenantId] UNIQUEIDENTIFIER NOT NULL, 
+    [InvoiceTypeCd] VARCHAR(20) NOT NULL, 
+    [ShortDescription] VARCHAR(50) NOT NULL, 
+    [CreateDate] DATETIME NOT NULL, 
+    [CreateUser] VARCHAR(50) NOT NULL, 
+    [ModifiedDate] DATETIME NULL, 
+    [ModifiedUser] VARCHAR(50) NULL
+)
