4c7240375d16d502a268efd7360514469758a3bc
Iftikhar Ali <i_syed2000@yahoo.com>
2019-12-05 17:36:08 -0500

    updates.

--- ISCJ/ISCJRegistration/dbo/Tables/ClassSession.sql
+++ ISCJ/ISCJRegistration/dbo/Tables/ClassSession.sql
@@ -2,12 +2,14 @@
     [ClassSessionId] UNIQUEIDENTIFIER NOT NULL,
     [ClassId]        UNIQUEIDENTIFIER NOT NULL,
     [InstructorId]   UNIQUEIDENTIFIER NOT NULL,
+	[SessionDate] DATETIME NOT NULL,
     [CreateUser]     VARCHAR (50)     NOT NULL,
     [CreateDate]     DATETIME         NOT NULL,
     [ModifiedUser]   VARCHAR (50)     NULL,
     [ModifiedDate]   DATETIME         NULL,
     [RowVersion]     ROWVERSION       NULL,
     [TenantId] UNIQUEIDENTIFIER NOT NULL, 
+    
     CONSTRAINT [PK_ClassSession] PRIMARY KEY CLUSTERED ([ClassSessionId] ASC)
 );
 
