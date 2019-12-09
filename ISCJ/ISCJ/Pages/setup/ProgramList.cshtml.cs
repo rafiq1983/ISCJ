d6b91efaabf41f1c477dabcd84d4d7689d42c761
Iftikhar Ali <i_syed2000@yahoo.com>
2019-12-09 10:19:42 -0500

    updates.  Navigation bar updates.

---
+++ ISCJ/ISCJ/Pages/setup/ProgramList.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLogic;
+using MA.Common.Entities.Registration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ISCJ.Pages.setup
+{
+    public class ProgramListModel : BasePageModel
+    {
+        ProgramManager mgr = new ProgramManager();
+
+        public void OnGet()
+        {
+            ProgramDetails = mgr.GetAllPrograms(GetCallContext());
+        }
+
+
+        public List<ProgramDetail> ProgramDetails
+        {
+            get;
+            private set;
+        }
+    }
+}
