96d48552af4695b66f9f36822e725fb1d5adcbc9
Iftikhar Ali <i_syed2000@yahoo.com>
2020-01-10 00:20:15 -0500

    Create "StudentCounter" upon organization setup. (Changes made in DEV branch)

--- ISCJ/ISCJRegistration/dbo/Tables/SequenceCounter.sql
+++ ISCJ/ISCJRegistration/dbo/Tables/SequenceCounter.sql
@@ -4,5 +4,9 @@
     [CounterType] VARCHAR(50) NOT NULL, 
     [TenantId] UNIQUEIDENTIFIER NULL, 
     [CounterValue] INT NOT NULL, 
-    CONSTRAINT [CK_SequenceCounter_CounterType] CHECK (CounterType = 'Global' or CounterType = 'Tenant')
+    [CounterName] VARCHAR(50) NOT NULL, 
+    CONSTRAINT [CK_SequenceCounter_CounterType] CHECK (CounterType = 'Global' or CounterType = 'Tenant'), 
+    CONSTRAINT [Unique_SequenceCounter_TenantId_Column] UNIQUE ([CounterName], [TenantId]) 
 )
+
+GO
