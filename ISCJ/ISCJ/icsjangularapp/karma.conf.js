4e423fe583786ed02c6843ad29f5eca2b69f0a53
Iftikhar Ali <i_syed2000@yahoo.com>
2021-11-24 22:04:03 -0500

    Added angular and vue apps

--- ISCJ/ISCJ/icsjangularapp/karma.conf.js
+++ ISCJ/ISCJ/icsjangularapp/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angularicsjapp'),
+      dir: require('path').join(__dirname, './coverage/icsjangularapp'),
       subdir: '.',
       reporters: [
         { type: 'html' },
